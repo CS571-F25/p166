cd30cc26aa728dede1fad56fe2271e884ac2ec53
neelk <neelk@MacBookPro.lan>
2025-11-19 20:39:01 -0600

    Fixed bug

--- src/pages/TeamsPage.jsx
+++ src/pages/TeamsPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import TeamCard from "../components/TeamCard.jsx";
-import { sampleTeams } from "../sampleData.jsx";
+import { sampleTeams } from "./sampleData.jsx";
 
 const TeamsPage = () => {
   return (
